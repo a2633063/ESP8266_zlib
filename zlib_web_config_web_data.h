0fcbb65a2ae3611c327805a1c6d698c0244d02d6
a2633063 <76966589@qq.com>
2021-10-14 16:16:12 +0800

    fix: web配网页面跳转至mqtt配置页面无效的问题

--- zlib_web_config_web_data.h
+++ zlib_web_config_web_data.h
@@ -4,6 +4,6 @@ extern const unsigned char web_mqttfail_html[2207];
 extern const unsigned char web_mqttsetting_html[2958];
 extern const unsigned char web_mqttsuccess_html[2428];
 extern const unsigned char web_wififail_html[2632];
-extern const unsigned char web_wifisetting_html[2915];
+extern const unsigned char web_wifisetting_html[2926];
 extern const unsigned char web_wifisuccess_html[2414];
 #endif
